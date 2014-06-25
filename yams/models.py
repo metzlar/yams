d013bf87e5ef8c231867621d7ad1a52b3d169755
Ivan Metzlar <metzlar@gmail.com>
2014-06-24 17:21:29 -0700

    remove django files

--- yams/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Machine(models.Model):
-    name = models.CharField(max_length=25)
-    ip = models.CharField(max_length=15)
-    last_known_state = models.CharField(max_length=5)
